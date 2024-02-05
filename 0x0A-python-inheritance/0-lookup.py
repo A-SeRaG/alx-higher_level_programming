929dc634b9ea4ffca3bd91061c248275103b3c1c
SeRaG <aserag0101@gmail.com>
2024-02-05 10:52:53 -0800

    programming

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module for lookup method'''
+
+def lookup(obj):
+    '''function looks up attributes and methods of an object
+    Args:
+        obj:object to list
+    Return:
+        list: list of attributes
+    '''
+    return dir(obj)
