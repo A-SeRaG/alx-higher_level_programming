ed23cfb5341bfeede6800d498426903d5057c543
SeRaG <aserag0101@gmail.com>
2024-02-05 11:30:58 -0800

    programming

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Module for same class.'''
+'''Module for is_same_class method.'''
 
 def is_same_class(obj, a_class):
     '''Test if the object is exactly an instance of the same class'''
