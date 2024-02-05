821add72b603970afe7b1cd3bad95ab77519afdf
SeRaG <aserag0101@gmail.com>
2024-02-05 11:05:03 -0800

    programming

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for MyList'''
+
+class MyList(List):
+    '''Custom MyList class'''
+    def print_sorted(self):
+        '''Method for printed sorted list'''
+        print(sorted(self))
