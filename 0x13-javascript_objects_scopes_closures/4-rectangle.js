da4e6804ce21298b23a5c382866cba4f5b8c99c4
SeRaG <aserag0101@gmail.com>
2024-04-09 14:55:51 -0700

    programming

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let s;
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    let a = this.height;
+    this.height = this.width;
+    this.width = a;
+    this.print();
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+    this.print();
+  }
+}
+
+module.exports = Rectangle;
