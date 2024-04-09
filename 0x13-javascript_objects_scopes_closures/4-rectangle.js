037ec95459c75dce321f922727c4593222c7cbd6
SeRaG <aserag0101@gmail.com>
2024-04-09 14:58:51 -0700

    programming

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,8 +8,8 @@ class Rectangle {
   }
 
   print () {
-    let s;
     for (let i = 0; i < this.height; i++) {
+      let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
@@ -18,7 +18,7 @@ class Rectangle {
   }
 
   rotate () {
-    let a = this.height;
+    const a = this.height;
     this.height = this.width;
     this.width = a;
   }
