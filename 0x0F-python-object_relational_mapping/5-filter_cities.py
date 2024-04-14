7875a9a96a6b9cc0e6436e10bf7696f7394f4067
SeRaG <aserag0101@gmail.com>
2024-04-14 10:38:41 -0700

    programming

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,42 @@
 #!/usr/bin/python3
 """
-script that takes in the name of a state as an argument and lists 
-all cities of that state, using the database hbtn_0e_4_usa
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
 """
-import MySQLdb
-import sys
 
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    con = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    c = con.cursor()
-    c.execute("SELECT cities.id, cities.name FROM cities \
-              JOIN states ON cities.state_id = states.id \
-              WHERE states.name LIKE BINARY %(s_name)s \
-              ORDER BY cities.id ASC", {'s_name': sys.argv[4]})
-    ss = c.fetchall()
-    if ss:
-        print(", ".join([s[1] for s in ss]))
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
+        rows_selected = db_cursor.fetchall()
+
+    if rows_selected is not None:
+        print(", ".join([row[1] for row in rows_selected]))
     c.close()
     con.close()
