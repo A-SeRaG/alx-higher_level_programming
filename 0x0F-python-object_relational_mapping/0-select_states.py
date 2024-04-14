fb3f318f0b1f341f42d42f97c9538d1b7a6e2d59
SeRaG <aserag0101@gmail.com>
2024-04-14 08:10:09 -0700

    programming

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                           passwd=sys.argv[2], db=sys.argv[3], port=3306,
                           charset="utf8")
     c = con.cursor()
-    c.excute("SELECT * FROM states ORDER BY id ASC")
+    c.execute("SELECT * FROM states ORDER BY id ASC")
     ss = c.fetchall()
     for s in ss:
         print(s)
