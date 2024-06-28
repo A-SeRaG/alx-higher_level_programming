8f37e9ce15ec893ce3225ebb3f2966e362cba76e
SeRaG <aserag0101@gmail.com>
2024-06-28 13:22:26 -0700

    programming

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
 """
-This script prints the first State object
-from the database `hbtn_0e_6_usa`.
+Script that prints the first State object from the database hbtn_0e_6_usa.
 """
-
-from sys import argv
-from model_state import State, Base
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
+    # Get MySQL credentials and database name from command line arguments
+    usrname = sys.argv[1]
+    passw = sys.argv[2]
+    dbname = sys.argv[3]
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
-
-    engine = create_engine(db_url, pool_pre_ping=True)
+    # Create a connection to the MySQL server
+    engine = create_engine(f'mysql+mysqldb://{usrname}:{passw}@localhost:3306/{dbname}', pool_pre_ping=True)
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
+    # Create a Session
     session = Session()
 
+    # Query the first State object
     state = session.query(State).order_by(State.id).first()
-    if state is not None:
-        print('{0}: {1}'.format(state.id, state.name))
+
+    # Display the result
+    if state:
+        print(f"{state.id}: {state.name}")
     else:
         print("Nothing")
+    
+    # Close the session
     session.close()
+
