af384f086fe3189c832499caa2a8da847c28269b
SeRaG <aserag0101@gmail.com>
2024-06-28 13:18:28 -0700

    programming

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(db_url)
+    engine = create_engine(db_url, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
 
     session = Session()
@@ -28,3 +28,4 @@ if __name__ == "__main__":
         print('{0}: {1}'.format(state.id, state.name))
     else:
         print("Nothing")
+    session.close()
