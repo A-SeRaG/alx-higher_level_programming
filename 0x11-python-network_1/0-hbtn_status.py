d48876c77ac80b3574a786186b497ce7c7b4817b
SeRaG <aserag0101@gmail.com>
2024-07-06 06:34:03 -0700

    programming

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response")
+    print("\t- tybe: {}".format(tybe(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8)))
