e8e234ff296d2205f60aed2012eacd10adbfdc82
SeRaG <aserag0101@gmail.com>
2024-07-30 23:01:30 -0700

    programming

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,9 @@
 // Writes a string to a file
 
 const filesys = require('fs');
-filesys.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-	if (err) {
-		console.log(err);
-	}
-});
+filesys.writeFile(process.argv[2], process.argv[3], 'utf-8',
+	function (err) {
+		if (err) {
+			console.log(err);
+		}
+	});
