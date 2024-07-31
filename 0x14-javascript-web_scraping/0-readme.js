af501d4274655233b1de5eea7a7516a96c643bd9
SeRaG <aserag0101@gmail.com>
2024-07-30 22:52:42 -0700

    programming

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-#Read file in UTF-8
+// Read from file
 
 const filesys = require('fs');
 filesys.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
